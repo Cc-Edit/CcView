eaa9487e944591505d99ac53ae2f49d0da51ab44
ccedit <ccedit@126.com>
2023-09-08 20:02:43 +0800

    first commit

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -7,7 +7,7 @@ export default function Copyright() {
     <Typography className={styles.copyright} variant='body2' color='text.secondary' align='center'>
       <Link target='_blank' underline='hover' rel='noopener noreferrer' color='inherit' href='https://beian.miit.gov.cn/'>
         京ICP备2023019680号
-      </Link>
+      </Link>&nbsp;&nbsp;
       <Link target='_blank' underline='hover' rel='noopener noreferrer' color='inherit' href='https://icons8.com/'>
         Icons by icons8
       </Link>
